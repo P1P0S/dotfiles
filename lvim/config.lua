ef24dfb636d2759860508f70bcfbb29a44623fbc
Pipos <62353660+P1P0S@users.noreply.github.com>
2022-04-13 01:58:59 -0300

    ✨Revamped with new colors and plugins
    Add: neosolarized, defx and tabline

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,33 +1,29 @@
--- - THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 vim.opt.mouse = "a"
 vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.expandtab = false
 
--- statusline default
-lvim.builtin.lualine.active = false
+-- plugins default
+lvim.builtin.lualine.active = true
 lvim.builtin.bufferline.active = false
--- lvim.builtin.lualine.style = "lvim" --default or none
--- lvim.builtin.lualine.options.theme = "gruvbox"
-
--- personal plugin staline.nvim
-vim.cmd("source ~/.config/lvim/user/staline.lua")
-vim.cmd("source ~/.config/lvim/user/stabline.lua")
+lvim.builtin.nvimtree.active = false
+-- lvim.builtin.lualine.style = "default" --default or none
+-- lvim.builtin.lualine.options.theme = "neosolarized"
 
 -- personal config
-lvim.transparent_window= true
-lvim.colorscheme = "tokyonight"
-vim.g.tokyonight_style = "night"
+lvim.colorscheme = "neosolarized"
+lvim.transparent_window = true
 lvim.builtin.gitsigns.opts.signs.add.text = ''
 lvim.builtin.gitsigns.opts.signs.change.text = ''
 lvim.builtin.gitsigns.opts.signs.delete.text = ''
 lvim.builtin.gitsigns.opts.signs.topdelete.text = ''
 lvim.builtin.gitsigns.opts.signs.changedelete.text = ''
-vim.cmd("source ~/.config/lvim/user/blankline.lua")
-vim.cmd("source ~/.config/lvim/user/colorizer.lua")
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -36,7 +32,10 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = false
 -- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+lvim.keys.normal_mode["te"] = ":tabedit<cr>"
+lvim.keys.normal_mode["<M-Left>"] = ":tabprevious<cr>"
+lvim.keys.normal_mode["<M-Right>"] = ":tabnext<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
@@ -78,18 +77,18 @@ lvim.builtin.nvimtree.show_icons.git = 1
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"tsx",
+	"css",
+	"rust",
+	"java",
+	"yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -110,74 +109,72 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
-lvim.lsp.on_attach_callback = function(client, bufnr)
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-end
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--     local function buf_set_option(...)
+--         vim.api.nvim_buf_set_option(bufnr, ...)
+--     end
+
+--     --Enable completion triggered by <c-x><c-o>
+--     buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
-  {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "80" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact", "javascript" },
-  },
+	{ command = "black", filetypes = { "python" } },
+	{ command = "isort", filetypes = { "python" } },
+	{
+		-- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+		command = "prettier",
+		---@usage arguments to pass to the formatter
+		-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+		extra_args = { "--print-with", "80" },
+		---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+		filetypes = { "typescript", "typescriptreact", "javascript" },
+	},
 }
 
 -- -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { command = "flake8", filetypes = { "python" } },
-  { command = "eslint", filetypes = { "javascript" } },
-  -- {
-  --   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --   command = "shellcheck",
-  --   ---@usage arguments to pass to the formatter
-  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --   extra_args = { "--severity", "warning" },
-  -- },
-  -- {
-  --   command = "codespell",
-  --   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --   filetypes = { "javascript", "python" },
-  -- },
+	{ command = "flake8", filetypes = { "python" } },
+	{ command = "eslint", filetypes = { "javascript" } },
+	{
+		-- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+		command = "shellcheck",
+		---@usage arguments to pass to the formatter
+		-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+		extra_args = { "--severity", "warning" },
+	},
+	{
+		command = "codespell",
+		---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+		filetypes = { "javascript", "python" },
+	},
 }
 
 -- Additional Plugins
 lvim.plugins = {
-    {"folke/tokyonight.nvim"},
-    {
-      "folke/trouble.nvim",
-      cmd = "TroubleToggle",
-    },
-    {"TaDaa/vimade"},
-    {"tamton-aquib/staline.nvim"},
-    {"norcalli/nvim-colorizer.lua"},
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufRead",
-    setup = function()
-      vim.g.indentLine_enabled = 1
-      vim.g.indent_blankline_char = "▏"
-      vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
-      vim.g.indent_blankline_buftype_exclude = {"terminal"}
-      vim.g.indent_blankline_show_trailing_blankline_indent = false 
-      vim.g.indent_blankline_show_first_indent_level = false 
-   end
-},
+	-- {
+	-- "folke/tokyonight.nvim",
+	-- cmd = "vim.g.tokyonight_style = 'night'",
+	--}
+	{ "svrana/neosolarized.nvim" },
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{ "TaDaa/vimade" },
+	{ "tamton-aquib/staline.nvim" },
+	{ "norcalli/nvim-colorizer.lua" },
+	{ "lukas-reineke/indent-blankline.nvim" },
+	{ "Shougo/defx.nvim"
+	},
+	{ "kristijanhusak/defx-icons" },
+	{ "kristijanhusak/defx-git" },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
-  { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+	{ "BufWinEnter", "*.lua", "setlocal ts=4 sw=4" },
 }
