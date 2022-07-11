929b2a0ae7cb251d2a2225c05d02c0aecd030e79
Pipos <62353660+P1P0S@users.noreply.github.com>
2022-07-11 14:34:54 -0300

    add aplha

--- nvchad/plugins/init.lua
+++ nvchad/plugins/init.lua
@@ -5,18 +5,20 @@ return {
       require "custom.plugins.null-ls"
     end,
   },
-
   ["editorconfig/editorconfig-vim"] = {
     disable = false,
   },
 
   ["goolord/alpha-nvim"] = {
     disable = false,
+    config = function()
+      require "custom.configs.alpha"
+    end
   },
   ["andweeb/presence.nvim"] = {
     disable = false,
-    config = function ()
+    config = function()
       require "custom.configs.presence"
     end
-   },
+  }
 }
