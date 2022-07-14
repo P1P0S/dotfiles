8e3eca74f7d90ab080bf62236227b91421b8084d
Marnei Machado <marnei.machado@gmail.com>
2022-07-13 22:20:54 -0300

    fix null-ls and lspconfig

--- nvchad/plugins/null-ls.lua
+++ nvchad/plugins/null-ls.lua
@@ -6,14 +6,21 @@ end
 
 local b = null_ls.builtins
 
-local sources = {
+local with_root_file = function(...)
+    local files = { ... }
+    return function(utils)
+        return utils.root_has_file(files)
+    end
+end
 
+local sources = {
    -- webdev stuff
-   b.formatting.deno_fmt,
-   b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "javascript" } },
+   b.formatting.prettier,
 
    -- Lua
-  b.formatting.stylua,
+   b.formatting.stylua.with({
+      condition = with_root_file("stylua.toml")
+   }),
 
    -- Shell
    b.formatting.shfmt,
@@ -23,15 +30,24 @@ local sources = {
    b.formatting.clang_format,
 }
 
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+
 null_ls.setup {
-  debug = true,
-  sources = sources,
+   debug = true,
+   sources = sources,
 
-  -- format on save
-  on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
-    end
-  end,
+   on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+         vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+               -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+               vim.lsp.buf.formatting_sync()
+            end,
+         })
+      end
+   end,
 }
-
