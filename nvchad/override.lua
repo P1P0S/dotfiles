ecc50de8df2ec06db5b736bd9130315d559e47c8
Marnei Machado <marnei.machado@gmail.com>
2022-07-15 21:44:22 -0300

    change 4 tabs to 2 tabs

--- nvchad/override.lua
+++ nvchad/override.lua
@@ -1,44 +1,44 @@
 local M = {}
 
 M.gitsigns = {
-   signs = {
-      add          = {hl = 'GitSignsAdd'   , text = '', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-      change       = {hl = 'GitSignsChange', text = '', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-      delete       = {hl = 'GitSignsDelete', text = '', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      topdelete    = {hl = 'GitSignsDelete', text = 'ﬢ', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      changedelete = {hl = 'GitSignsChange', text = '', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-   },
+  signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = 'ﬢ', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
 }
 
 
 M.nvimtree = {
-   git = {
-      enable = true,
-   },
+  git = {
+    enable = true,
+  },
 
-   renderer = {
-      highlight_git = true,
-      icons = {
-         show = {
-            git = true,
-         },
-         glyphs = {
-            folder = {
-               default = "",
-               open = "",
-               empty = "",
-               empty_open = "",
-               symlink = "",
-               symlink_open = "",
-            },
-         },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
       },
-   },
-   actions = {
-      open_file = {
-         quit_on_open = true,
+      glyphs = {
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
       },
-   },
+    },
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
 }
 
 M.blankline = {
