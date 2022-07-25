3b72d9b9c275a530ffb8d27d418fb0c9fd2a8729
Marnei Machado <marnei.machado@gmail.com>
2022-07-25 20:45:21 -0300

    fix: lsp custom servers

--- nvchad/plugins/lspconfig.lua
+++ nvchad/plugins/lspconfig.lua
@@ -1,14 +1,23 @@
--- tsserver
-require("custom.configs.tsserver")
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 local servers = { "html", "cssls", "clangd", "jsonls", "eslint", "tsserver", "emmet_ls" }
 
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup({
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--   })
+-- end   
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
+  lspconfig[lsp].setup {
+    on_attach = attach,
     capabilities = capabilities,
-  })
+    root_dir = vim.loop.cwd,
+  }
 end
+
+-- temporarily disable tsserver suggestions
+require("custom.configs.tsserver")
+
