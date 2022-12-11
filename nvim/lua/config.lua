51768dc1f7f8be53cb09c2aeb4ac15ba002dbcd9
Marnei Machado <marnei.machado@gmail.com>
2022-12-10 22:23:25 -0300

    add some settings

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,46 +1,51 @@
 -- vim.opt = set
 -- vim.g = let
+local opt = vim.opt
+local g = vim.g
+local wo = vim.wo
 
 -- nightfly theme
-vim.g.nightflyCursorColor = true
-vim.g.nightflyNormalFloat = true
-vim.g.nightflyUnderlineMatchParen = true
+g.nightflyCursorColor = true
+g.nightflyNormalFloat = true
+g.nightflyUnderlineMatchParen = true
 
 vim.cmd("autocmd!")
 
 vim.scriptencoding = "utf-8"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-vim.opt.fileformat = unix
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+opt.fileformat = "unix"
 
-vim.wo.number = true
-vim.wo.rnu = true
-vim.g.mapleader = " "
+--vim.wo.number = true
+wo.rnu = true
+g.mapleader = " "
 
-vim.opt.ruler = true
-vim.opt.cursorline = true
-vim.opt.mouse = "a"
-vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = true
-vim.opt.backup = false
-vim.opt.showcmd = true
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 2
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.shell = "pwsh"
-vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
-vim.opt.inccommand = "split"
-vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.nuw = 1
-vim.opt.tabstop = 4
-vim.opt.wrap = false -- No Wrap lines
-vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
-vim.opt.wildignore:append({ "*/node_modules/*" })
-vim.opt.clipboard:prepend({ "unnamed", "unnamedplus" })
+opt.ruler = true
+opt.cursorline = true
+opt.mouse = "a"
+opt.title = true
+opt.autoindent = true
+opt.smartindent = true
+opt.hlsearch = true
+opt.backup = false
+opt.showcmd = true
+opt.cmdheight = 1
+opt.laststatus = 2
+opt.expandtab = true
+opt.scrolloff = 10
+opt.shell = "pwsh"
+opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+opt.inccommand = "split"
+opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+opt.smarttab = true
+opt.breakindent = true
+opt.shiftwidth = 2
+opt.nuw = 1
+opt.tabstop = 4
+opt.wrap = false -- No Wrap lines
+opt.backspace = { "start", "eol", "indent" }
+opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+opt.wildignore:append({ "*/node_modules/*" })
+opt.clipboard:prepend({ "unnamed", "unnamedplus" })
+--opt.guicursor = ""
+opt.colorcolumn = "90"
