11a076419ffbf0d95896da2cbe045a230dd6522f
Marnei Machado <marnei.machado@gmail.com>
2022-12-12 20:15:32 -0300

    remove useless code

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,19 +1,9 @@
 local nvim_lsp = require("lspconfig")
 local wk = require("which-key")
 
-local on_attach = function(client, bufnr)
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-
+local on_attach = function()
 	-- Mappings
 	local opts = { mode = "n", prefix = "<leader>", noremap = true, silent = true }
-
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	--buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	--buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-	--buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	--buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
 
 	wk.register({
 		l = {
@@ -21,10 +11,6 @@ local on_attach = function(client, bufnr)
 			i = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Open File Implementation" },
 		},
 	}, opts)
-
-	-- Disable Autoformat deprecated on 0.8
-	--client.resolved_capabilities.document_formatting = false
-	--client.resolved_capabilities.document_range_formatting = false
 end
 
 local servers = { "tsserver", "sumneko_lua" }
