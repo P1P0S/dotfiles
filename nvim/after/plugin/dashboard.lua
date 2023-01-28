90bf74181319c461cc7261f8553a97edd4910848
Marnei Machado <marnei.machado@gmail.com>
2023-01-28 11:30:25 -0300

    update dashboard config

--- nvim/after/plugin/dashboard.lua
+++ nvim/after/plugin/dashboard.lua
@@ -3,44 +3,43 @@ if not status then
   return
 end
 
-db.custom_header = {
-  "",
-  " ██▓███    ██▓ ██▓███   ▒█████    ██████ ",
-  "▓██░  ██ ▒▓██▒▓██░  ██ ▒██▒  ██▒▒██    ▒ ",
-  "▓██░ ██▓▒▒▒██▒▓██░ ██▓▒▒██░  ██▒░ ▓██▄   ",
-  "▒██▄█▓▒ ▒░░██░▒██▄█▓▒ ▒▒██   ██░  ▒   ██▒",
-  "▒██▒ ░  ░░░██░▒██▒ ░  ░░ ████▓▒░▒██████▒▒",
-  "▒▓▒░ ░  ░ ░▓  ▒▓▒░ ░  ░░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░",
-  "░▒ ░     ░ ▒ ░░▒ ░       ░ ▒ ▒░ ░ ░▒  ░ ░",
-  "░░       ░ ▒ ░░░       ░ ░ ░ ▒  ░  ░  ░  ",
-  "           ░               ░ ░        ░  ",
-  "",
-}
-
-db.custom_center = {
-  {
-    icon = "  ",
-    desc = "Find Files                  ",
-    action = "Telescope find_files find_command=rg,--hidden,--files",
+db.setup({
+  theme = "hyper",
+  config = {
+    week_header = {
+      enable = true,
+    },
+    shortcut = {
+      {
+        desc = "󰗼 Close",
+        group = "DiagnosticHint",
+        action = "qa!",
+        key = "q",
+      },
+      {
+        desc = " Update",
+        group = "@property",
+        action = "PackerUpdate",
+        key = "u",
+      },
+      {
+        desc = " Files",
+        group = "Label",
+        action = "Telescope find_files cwd=",
+        key = "f",
+      },
+      {
+        desc = " Apps",
+        group = "DiagnosticHint",
+        action = "Telescope app",
+        key = "a",
+      },
+      {
+        desc = " Settings",
+        group = "Number",
+        action = ":e $MYVIMRC | cd %:p:h",
+        key = "d",
+      },
+    },
   },
-  {
-    icon = "  ",
-    desc = "Rencent Files               ",
-    action = "Telescope oldfiles",
-  },
-  {
-    icon = "  ",
-    desc = "Bookmarks                   ",
-    action = "Telescope marks",
-  },
-  {
-    icon = "  ",
-    desc = "Themes                      ",
-    action = "Telescope colorscheme",
-  },
-  {
-    icon = "  ",
-    desc = "Settings                    ",
-    action = ":e $MYVIMRC | :cd %:p:h",
-  },
-}
+})
