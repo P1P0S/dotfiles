4f14318f49d4c438b317fd7678ce7bea9851aeac
Marnei Machado <marnei.machado@gmail.com>
2023-01-29 19:53:13 -0300

    update lspsaga

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -15,21 +15,5 @@ lspsaga.setup({
     -- border type can be single,double,rounded,solid,shadow.
     border = "rounded",
     winblend = 5,
-    colors = {
-      --float window normal bakcground color
-      normal_bg = "#002b36",
-      --title background color
-      title_bg = "#afd700",
-      red = "#e95678",
-      magenta = "#b33076",
-      orange = "#FF8700",
-      yellow = "#f7bb3b",
-      green = "#afd700",
-      cyan = "#36d0e0",
-      blue = "#61afef",
-      purple = "#CBA6F7",
-      white = "#d1d4cf",
-      black = "#1c1c19",
-    },
   },
 })
