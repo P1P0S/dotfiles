0c9f1f345112f84baf0d554007f4be7f0a95863a
Marnei Machado <marnei.machado@gmail.com>
2023-02-14 19:00:23 -0300

    update sumneko to lua_ls

--- nvim/lua/utils/lists.lua
+++ nvim/lua/utils/lists.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.lspservers = { "tsserver", "lua_ls", "html", "tailwindcss" }
-M.install_lspservers = { "tsserver", "sumneko_lua", "html", "tailwindcss" }
+M.install_lspservers = { "tsserver", "lua_ls", "html", "tailwindcss" }
 M.null_ls_fixers = { "eslint_d", "stylua", "prettier" }
 
 return M
