9e6dcfb28772105c7055f65b349f9833565db3b7
Marnei Machado <marnei.machado@gmail.com>
2023-02-15 17:46:37 -0300

    fix luasnip jump on tab

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -50,12 +50,12 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         }),
-        ["<Tab>"] = cmp.mapping(function()
-          luasnip.jump(1)
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function()
-          luasnip.jump(1)
-        end, { "i", "s" }),
+        -- ["<Tab>"] = cmp.mapping(function()
+        --   luasnip.jump(1)
+        -- end, { "i", "s" }),
+        -- ["<S-Tab>"] = cmp.mapping(function()
+        --   luasnip.jump(1)
+        -- end, { "i", "s" }),
       }),
 
       sources = cmp.config.sources({
@@ -85,6 +85,18 @@ return {
   highlight! CmpItemAbbrDeprecated guibg=NONE 
   highlight! CmpItemAbbrMatch guibg=NONE guifg=#5E97EC
   highlight! link CmpItemAbbrMatchFuzzy CmpItemAbbrMatch
+  " press <Tab> to expand or jump in a snippet. These can also be mapped separately
+  " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
+  imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+  " -1 for jumping backwards.
+  inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+
+  snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
+  snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+
+  " For changing choices in choiceNodes (not strictly necessary for a basic setup).
+  imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+  smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
 ]])
   end,
   dependencies = {
