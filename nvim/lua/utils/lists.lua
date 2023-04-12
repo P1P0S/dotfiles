d07a22483b1938a6537978a11880178cf73d2f05
Marnei Machado <marnei.machado@gmail.com>
2023-04-12 09:46:04 -0300

    add emmet_ls

--- nvim/lua/utils/lists.lua
+++ nvim/lua/utils/lists.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.lspservers = { "tsserver", "lua_ls", "html", "tailwindcss" }
+M.lspservers = { "tsserver", "lua_ls", "html", "tailwindcss", "emmet_ls" }
 M.install_lspservers = { "tsserver", "lua_ls", "html", "tailwindcss" }
 M.null_ls_fixers = { "eslint_d", "stylua", "prettier" }
 
