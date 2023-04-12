d3c48bc58de6ecaeb968380c46c92d6b67e781c0
Marnei Machado <marnei.machado@gmail.com>
2023-04-12 09:46:40 -0300

    fix space for gitlens

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -25,7 +25,7 @@ return {
 
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
     end
 
     vim.diagnostic.config({
