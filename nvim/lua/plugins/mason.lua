44821c12462b8dcae1c0777f9f9e319f2c47366b
Marnei Machado <marnei.machado@gmail.com>
2023-04-18 19:21:48 -0300

    fix mason config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -21,14 +21,5 @@ return {
         })
       end,
     },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      config = function()
-        require("mason-lspconfig").setup({
-          ensure_installed = require("utils.lists").install_lspservers,
-          automatic_installation = true,
-        })
-      end,
-    },
   },
 }
