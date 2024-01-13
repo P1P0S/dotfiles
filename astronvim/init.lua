bffcba0ccace9285f9f021798fa40bfdf16c6668
Pipos <62353660+P1P0S@users.noreply.github.com>
2024-01-13 20:09:15 -0300

    add theme configuration and neovide integration

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -1,6 +1,24 @@
+---@diagnostic disable: undefined-global
+local neovide = require "user.utils.neovide"
 local discipline = require "user.utils.discipline"
 discipline.cowboy()
 
+local themes = {
+  maachiato = "catppuccin-macchiato",
+  mocha = "catppuccin-mocha",
+  night = "tokyonight-night",
+  storm = "tokyonight-storm",
+  moon = "tokyonight-moon",
+  wave = "kanagawa-wave",
+  dragon = "kanagawa-dragon",
+  synthwave84,
+  everforest,
+  neg,
+}
+
+local selected_theme = "everforest"
+
 return {
-  --colorscheme = "catppuccin-macchiato",
+  colorscheme = themes[selected_theme] or selected_theme,
+  neovide,
 }
